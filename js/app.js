db2d4ec99fc006729b964cd4d3a6c68e17f7797d
ZingerY <Vitaliks90@mail.ru>
2018-08-27 01:51:22 +0300

    Правка отображения перков 2 уровня

--- js/app.js
+++ js/app.js
@@ -785,7 +785,7 @@ function listperkup(){
 //Создание списка перков в окне доступных перков
 function createlistperk() {
 	$("#crlistperk").html("");	
-	var mperk = {3: [],6: [],9: [],12: [],15: [],18: [],30: [],33: [],51: []};
+	var mperk = {2: [],6: [],9: [],12: [],15: [],18: [],30: [],33: [],51: []};
     var s = 0;
     if(!("tperk" in mychar)) 
         mychar["tperk"] = {};
