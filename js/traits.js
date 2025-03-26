5bf117130b90d77b0598fa3376b17aa8e414d39e
DeMhechev <124237836+DeMhechev@users.noreply.github.com>
2025-03-26 21:46:56 +0300

    UPDATE-PRE-34

--- js/traits.js
+++ js/traits.js
@@ -1,46 +1,60 @@
 var traits = {
-	// Быстрый метаболизм
+	// Быстрый метаболизм 34season
+	// За любое изменение ОЖ дается +1 ОЖ. +15нр при исцелении Суперстимпаком. +25 Уровень Лечения. 
+	// Яд выводится из организма в два раза быстрее. Устойчивость к яду и Устойчивость к радиации уменьшается в три раза.
 	TRAIT_FAST_METABOLISM: function(str){
 			if(!mychar.traits[str] && charp.tagt>0) {
 				//pr.add("feats","levh",15);
 				// pre 33:
-				pr.add("feats","levh",5);
+				pr.add("feats","levh",25);
 				mychar.traits[str] = 1;
+				charp.kostyltrait +=1;
 				charp.tagt--;	}
 			else if(mychar.traits[str] && charp.tagt<2) {
-				pr.add("feats","levh",-5);
+				pr.add("feats","levh",-25);
 				delete mychar.traits[str];
+				charp.kostyltrait-=1;
 				charp.tagt++;	}
 			},
 	// Крушила
+	// 34season
+	// +5 подавы за каждый удар, не снижает -1 од, не дает тагнуть метлу легкое тяж энерго ремонт 
 	TRAIT_BRUISER: function(str){
 			if(!mychar.traits[str] && charp.tagt>0) {
 				mychar.stats.STR[0]+=3;
-				pr.add("feats","apoi",-1);
+				//pr.add("feats","apoi",-1);
 				pr.add("feats","mdmg",25);
 				//pr.add("feats","critr",-30);
 				mychar.traits[str] = 1;
+				charp.kostylbruiser34 += 1;
 				charp.tagt--;	}
 			else if(mychar.traits[str] && charp.tagt<2) {
 				mychar.stats.STR[0]-=3;
-				pr.add("feats","apoi",1);
+				//pr.add("feats","apoi",1);
 				pr.add("feats","mdmg",-25);
 				//pr.add("feats","critr",30);
 				delete mychar.traits[str];
+				charp.kostylbruiser34 -= 1;
 				charp.tagt++;	}
 			},
 	// Хилое тело
+	// + 1 ловкость + 5 уворота + ловкость к АС +25 хп за уворот
+	// чутка багуется когда после взятия хилого тела берешь еще ловкости и потом убираешь.
 	TRAIT_SMALL_FRAME: function(str){
 			if(!mychar.traits[str] && charp.tagt>0) {
 				mychar.stats.AGI[0]+=1;
+				pr.add("feats","dodge",5)
+				pr.add("feats","armc",mychar.stats.AGI[0])
 				mychar.traits[str] = 1;
 				charp.tagt--;	}
 			else if(mychar.traits[str] && charp.tagt<2) {
+				pr.add("feats","armc",-mychar.stats.AGI[0])
 				mychar.stats.AGI[0]-=1;
+				pr.add("feats","dodge",-5)
 				delete mychar.traits[str];
 				charp.tagt++;	}
 			},
-	//	Однорукий
+	//	Однорукий 34 season -60 навыка
 	TRAIT_ONE_HANDER: function(str){
 			if(!mychar.traits[str] && charp.tagt>0) {
 				mychar.traits[str] = 1;
@@ -50,14 +64,14 @@ var traits = {
 				charp.tagt++;
 			}
 			},
-	// Точность
+	// Точность 34 season не дает 10 крита а +15% к мин макс урону
 	TRAIT_FINESSE: function(str){
 			if(!mychar.traits[str] && charp.tagt>0){
-				pr.add("feats","crit",10);
+				// pr.add("feats","crit",10);
 				mychar.traits[str] = 1;
 				charp.tagt--;	}
 			else if(mychar.traits[str] && charp.tagt<2){
-				pr.add("feats","crit",-10);
+				//pr.add("feats","crit",-10);
 				delete mychar.traits[str];
 				charp.tagt++;
 			}
@@ -74,7 +88,9 @@ var traits = {
 				charp.tagt++;
 			}
 			},
-	// Вор (вместо Громилы)
+	// Вор (вместо Громилы) 34 season
+	// Холодное одноручное оружие и метательные ножи наносят +10 конечного урона, но точность другого оружия равна 5%. Бонусы для вора кулдаун=(навык/15), срезает 3 ОД у жертвы, кладет в нокдаун.
+	//	Вы не можете наносить критические удары. Исключает атакующие перки (кроме доп рукопашных повреждений), Взрывотехника и Боевого инженера.  Добавляет +15 к Уровню лечения
 	TRAIT_HEAVY_HANDED: function(str){
 			if(!mychar.traits[str] && charp.tagt>0){
 				pr.add("feats","levh",15);
@@ -87,7 +103,7 @@ var traits = {
 			}
 			},
 			// pre 33: нерф вора 15 регена вместо 25
-	// Быстрый стрелок
+	// Быстрый стрелок 34season описание поменять
 	TRAIT_FAST_SHOT: function(str){
 			if(!mychar.traits[str] && charp.tagt>0){
 				mychar.traits[str] = 1;
@@ -97,15 +113,15 @@ var traits = {
 				charp.tagt++;
 			}
 			},
-	// Маньяк
+	// Маньяк 34season +150 отлета +5 сек бег в бою
 	TRAIT_BLOODY_MESS: function(str){
 			if(!mychar.traits[str] && charp.tagt>0){
-				pr.add("skills","speed",125,1);
+				pr.add("skills","speed",150,1);
 				//pr.add("feats","live",-25);
 				mychar.traits[str] = 1;
 				charp.tagt--;	}
 			else if(mychar.traits[str] && charp.tagt<2){
-				pr.add("skills","speed",-125,1);
+				pr.add("skills","speed",-150,1);
 				//pr.add("feats","live",25);
 				delete mychar.traits[str];
 				charp.tagt++;
@@ -151,14 +167,14 @@ var traits = {
 				charp.tagt++;
 			}
 			},
-	// Химик
+	// Химик 34 session
 	TRAIT_CHEM_RELIANT: function(str){
 			if(!mychar.traits[str] && charp.tagt>0){
-				pr.add("feats", "live", 30);
+				//pr.add("feats", "live", 30);
 				mychar.traits[str] = 1;
 				charp.tagt--;	}
 			else if(mychar.traits[str] && charp.tagt<2){
-				pr.add("feats", "live", -30);
+				//pr.add("feats", "live", -30);
 				delete mychar.traits[str];
 				charp.tagt++;
 			}
@@ -180,29 +196,31 @@ var traits = {
 			if(!mychar.traits[str] && charp.tagt>0){
 				pr.add("feats","maxl",50);
 				mychar.traits[str] = 1;
+				charp.kostyltrait+=1;
 				charp.tagt--;	}
 			else if(mychar.traits[str] && charp.tagt<2){
 				pr.add("feats","maxl",-50);
 				delete mychar.traits[str];
-				charp.tagt++;
+				charp.kostyltrait-=1;
+				charp.tagt++;		
 			}
 			},
 	// Умелец
 	TRAIT_SKILLED: function(str){
 			if(!mychar.traits[str] && charp.tagt>0){
-				mychar.stats.CHA[0] += 2;
-				mychar.stats.ENU[0] += 2;
-				mychar.stats.INT[0] += 2;
-				mychar.stats.AGI[0] += 2;
+				mychar.stats.CHA[0] += 1;
+				mychar.stats.ENU[0] += 1;
+				mychar.stats.INT[0] += 1;
+				mychar.stats.AGI[0] += 1;
 				mychar.traits[str] = 1;
 				charp.tags++;
 				numbers($("#point2"),charp.tags);
 				charp.tagt--;	}
 			else if(mychar.traits[str] && charp.tagt<2){
-				mychar.stats.CHA[0] -= 2;
-				mychar.stats.ENU[0] -= 2;
-				mychar.stats.INT[0] -= 2;
-				mychar.stats.AGI[0] -= 2;
+				mychar.stats.CHA[0] -= 1;
+				mychar.stats.ENU[0] -= 1;
+				mychar.stats.INT[0] -= 1;
+				mychar.stats.AGI[0] -= 1;
 				delete mychar.traits[str];
 				charp.tags--;
 				numbers($("#point2"),charp.tags);
